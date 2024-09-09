7e125d8526c8d38137c8e940191aa96f5b2ecaf8
Aydan <91035120+Aydanjb@users.noreply.github.com>
2024-09-09 16:24:24 -0400

    added length form

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,4 +1,34 @@
 function App() {
+    return (
+        <div>
+            <h1>Unit Converter</h1>
+            <form>
+                <div>
+                    <label htmlFor="length">Enter length to convert: </label>
+                    <input name="length" id="length" />
+                </div>
+                <div>
+                    <label htmlFor="from">Unit to convert from: </label>
+                    <select name="from" id="from">
+                        <option value="Millimeters">Millimeters</option>
+                        <option value="Centimeters">Centimeters</option>
+                        <option value="Meters">Meters</option>
+                        <option value="Kilometers">Kilometers</option>
+                    </select>
+                </div>
+                <div>
+                    <label htmlFor="to">Unit to convert to: </label>
+                    <select name="to" id="to">
+                        <option value="Millimeters">Millimeters</option>
+                        <option value="Centimeters">Centimeters</option>
+                        <option value="Meters">Meters</option>
+                        <option value="Kilometers">Kilometers</option>
+                    </select>
+                </div>
+                <input type="submit" value="Convert" />
+            </form>
+        </div>
+    )
 }
 
 export default App
