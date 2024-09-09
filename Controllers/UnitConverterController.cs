77fdd1a2e1b6cbb9ef329e1783ab7ebe7fcc4c93
Aydan <91035120+Aydanjb@users.noreply.github.com>
2024-09-09 17:36:41 -0400

    Added POST endpoint and CORS

---
+++ Controllers/UnitConverterController.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UnitConverter.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UnitConverterController : Controller
+{
+    [HttpPost]
+    public int Test()
+    {
+        return 2;
+    }
+}
